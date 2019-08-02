fb2dccf6515ccb421c8e228fa3a720df233d465f
Jaume Viñas Navas <jaumevn@icloud.com>
2019-08-02 12:38:02 +0200

    Xcode project init

---
+++ XColor/XColor.h
@@ -0,0 +1,19 @@
+//
+//  XColor.h
+//  XColor
+//
+//  Created by Jaume Viñas Navas on 02/08/2019.
+//  Copyright © 2019 Jaume Viñas Navas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XColor.
+FOUNDATION_EXPORT double XColorVersionNumber;
+
+//! Project version string for XColor.
+FOUNDATION_EXPORT const unsigned char XColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XColor/PublicHeader.h>
+
+
